9550b2938cbda1b3af51883b77547f28b5b48ce7
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-11-19 19:41:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+
+  const arr = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) === " ") {
+      return arr.join("");
+    }
+    arr.push(s.charAt(i));
+  }
+
+  return arr.join("");
 }
 
 // Do not change the code below
